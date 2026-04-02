179d7e7a88748299f3629f4510d5145dae04f7bd
John Sirois <john.sirois@gmail.com>
2026-04-02 09:47:23 -0700

    Upgrade sha2 and other deps.
    Supercedes #372.

--- src/main.rs
+++ src/main.rs
@@ -365,7 +365,7 @@ mod tests {
         assert_eq!(1205568, buffer.len());
         assert_eq!(
             "937683255e98caf10745a674d7063bd38e9cbeb523b9f8ef4dbe8807abc35382".to_string(),
-            format!("{digest:x}", digest = Sha256::digest(buffer))
+            hex::encode(Sha256::digest(buffer))
         );
     }
 
